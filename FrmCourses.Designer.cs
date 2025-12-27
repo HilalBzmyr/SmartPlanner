1bb46aa76f2fe2bbfb168f604fddc7b1e878df1c
Hill <hilal.bizimyer@stu.fbu.edu.tr>
2025-12-27 13:33:46 +0300

    Add project files.

---
+++ FrmCourses.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace smartPlanner
+{
+    partial class FrmCourses
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAddCourse = new Button();
+            btnUpdateCourse = new Button();
+            btnDeleteCourse = new Button();
+            btnClear = new Button();
+            label1 = new Label();
+            txtCourseName = new TextBox();
+            label2 = new Label();
+            txtInstructorName = new TextBox();
+            dgvCourses = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvCourses).BeginInit();
+            SuspendLayout();
+            // 
+            // btnAddCourse
+            // 
+            btnAddCourse.Location = new Point(383, 85);
+            btnAddCourse.Name = "btnAddCourse";
+            btnAddCourse.Size = new Size(94, 29);
+            btnAddCourse.TabIndex = 0;
+            btnAddCourse.Text = "Add";
+            btnAddCourse.UseVisualStyleBackColor = true;
+            btnAddCourse.Click += btnAddCourse_Click;
+            // 
+            // btnUpdateCourse
+            // 
+            btnUpdateCourse.Location = new Point(523, 140);
+            btnUpdateCourse.Name = "btnUpdateCourse";
+            btnUpdateCourse.Size = new Size(94, 29);
+            btnUpdateCourse.TabIndex = 1;
+            btnUpdateCourse.Text = "Update";
+            btnUpdateCourse.UseVisualStyleBackColor = true;
+            btnUpdateCourse.Click += btnUpdateCourse_Click;
+            // 
+            // btnDeleteCourse
+            // 
+            btnDeleteCourse.Location = new Point(650, 89);
+            btnDeleteCourse.Name = "btnDeleteCourse";
+            btnDeleteCourse.Size = new Size(94, 29);
+            btnDeleteCourse.TabIndex = 2;
+            btnDeleteCourse.Text = "Delete";
+            btnDeleteCourse.UseVisualStyleBackColor = true;
+            btnDeleteCourse.Click += btnDeleteCourse_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(523, 91);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(94, 29);
+            btnClear.TabIndex = 3;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(34, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Course Name:";
+            label1.Click += label1_Click;
+            // 
+            // txtCourseName
+            // 
+            txtCourseName.Location = new Point(162, 46);
+            txtCourseName.Name = "txtCourseName";
+            txtCourseName.Size = new Size(125, 27);
+            txtCourseName.TabIndex = 5;
+            txtCourseName.TextChanged += txtCourseName_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(34, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Instructor:";
+            // 
+            // txtInstructorName
+            // 
+            txtInstructorName.Location = new Point(162, 91);
+            txtInstructorName.Name = "txtInstructorName";
+            txtInstructorName.Size = new Size(125, 27);
+            txtInstructorName.TabIndex = 7;
+            txtInstructorName.TextChanged += txtInstructorName_TextChanged;
+            // 
+            // dgvCourses
+            // 
+            dgvCourses.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCourses.Dock = DockStyle.Bottom;
+            dgvCourses.Location = new Point(0, 233);
+            dgvCourses.Name = "dgvCourses";
+            dgvCourses.RowHeadersWidth = 51;
+            dgvCourses.Size = new Size(800, 217);
+            dgvCourses.TabIndex = 8;
+            dgvCourses.CellContentClick += dgvCourses_CellContentClick;
+            // 
+            // FrmCourses
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvCourses);
+            Controls.Add(txtInstructorName);
+            Controls.Add(label2);
+            Controls.Add(txtCourseName);
+            Controls.Add(label1);
+            Controls.Add(btnClear);
+            Controls.Add(btnDeleteCourse);
+            Controls.Add(btnUpdateCourse);
+            Controls.Add(btnAddCourse);
+            Name = "FrmCourses";
+            Text = "FrmCourses";
+            Load += FrmCourses_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvCourses).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAddCourse;
+        private Button btnUpdateCourse;
+        private Button btnDeleteCourse;
+        private Button btnClear;
+        private Label label1;
+        private TextBox txtCourseName;
+        private Label label2;
+        private TextBox txtInstructorName;
+        private DataGridView dgvCourses;
+    }
+}
---
+++ FrmCourses.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace smartPlanner
+{
+    public partial class FrmCourses : Form
+    {
+        // Connection string (App.config içindeki "connStr" ile aynı isim olmalı)
+        private string connStr = ConfigurationManager
+            .ConnectionStrings["connStr"].ConnectionString;
+
+        // Seçili CourseID'yi tutmak için
+        private int selectedCourseId = -1;
+
+        public FrmCourses()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCourses_Load(object sender, EventArgs e)
+        {
+            LoadCourses();
+        }
+
+        // Courses tablosunu DataGridView'e yükleyen metod
+        private void LoadCourses()
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                string query = "SELECT CourseID, CourseName, InstructorName FROM Courses";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvCourses.DataSource = dt;
+            }
+
+            // Sütun başlıkları daha okunaklı olsun
+            if (dgvCourses.Columns.Count > 0)
+            {
+                dgvCourses.Columns["CourseID"].HeaderText = "ID";
+                dgvCourses.Columns["CourseName"].HeaderText = "Course Name";
+                dgvCourses.Columns["InstructorName"].HeaderText = "Instructor";
+            }
+        }
+
+        // TextBox'ları temizleyen metod
+        private void ClearInputs()
+        {
+            txtCourseName.Clear();
+            txtInstructorName.Clear();
+            selectedCourseId = -1;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearInputs();
+        }
+
+        private void btnAddCourse_Click(object sender, EventArgs e)
+        {
+            // Basit validation: CourseName boşsa ekleme
+            if (string.IsNullOrWhiteSpace(txtCourseName.Text))
+            {
+                MessageBox.Show("Course Name cannot be empty.", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                string query = "INSERT INTO Courses (CourseName, InstructorName) " +
+                               "VALUES (@name, @inst)";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@name", txtCourseName.Text);
+                cmd.Parameters.AddWithValue("@inst",
+                    string.IsNullOrWhiteSpace(txtInstructorName.Text)
+                        ? (object)DBNull.Value
+                        : txtInstructorName.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            LoadCourses();
+            ClearInputs();
+
+        }
+
+        private void btnUpdateCourse_Click(object sender, EventArgs e)
+        {
+            if (selectedCourseId == -1)
+            {
+                MessageBox.Show("Please select a course from the list to update.", "Info",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtCourseName.Text))
+            {
+                MessageBox.Show("Course Name cannot be empty.", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                string query = "UPDATE Courses " +
+                               "SET CourseName = @name, InstructorName = @inst " +
+                               "WHERE CourseID = @id";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@name", txtCourseName.Text);
+                cmd.Parameters.AddWithValue("@inst",
+                    string.IsNullOrWhiteSpace(txtInstructorName.Text)
+                        ? (object)DBNull.Value
+                        : txtInstructorName.Text);
+                cmd.Parameters.AddWithValue("@id", selectedCourseId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            LoadCourses();
+            ClearInputs();
+        }
+
+        private void btnDeleteCourse_Click(object sender, EventArgs e)
+        {
+            if (selectedCourseId == -1)
+            {
+                MessageBox.Show("Please select a course from the list to delete.", "Info",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            var result = MessageBox.Show("Are you sure you want to delete this course?",
+                "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result != DialogResult.Yes)
+                return;
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                string query = "DELETE FROM Courses WHERE CourseID = @id";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", selectedCourseId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            LoadCourses();
+            ClearInputs();
+
+        }
+
+        private void txtCourseName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtInstructorName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvCourses_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return; // header'a tıklanmışsa
+
+            DataGridViewRow row = dgvCourses.Rows[e.RowIndex];
+
+            selectedCourseId = Convert.ToInt32(row.Cells["CourseID"].Value);
+            txtCourseName.Text = row.Cells["CourseName"].Value.ToString();
+            txtInstructorName.Text = row.Cells["InstructorName"].Value.ToString();
+
+        }
+    }
+}
